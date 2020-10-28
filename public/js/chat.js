249340c4d654bd63ea877e2410fd1ca978c4263a
DungLu <quangdunglu159@gmail.com>
2020-10-28 16:05:28 +0700

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,34 @@
+$(function () {
+    //Kết nối tới server socket đang lắng nghe
+    var socket = io.connect('http://localhost:7778');
+
+    //Socket nhận data và append vào giao diện
+    socket.on("send", function (data) {
+        console.log(data);
+        $("#content").append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    //Bắt sự kiện click gửi message
+    $("#sendMessage").on('click', function () {
+        sendMessage();
+    })
+
+    $("#message").on('keypress', function (e) {
+        if (e.which == 13) {
+            sendMessage();
+        }
+    });
+
+    var sendMessage = function () {
+        var username = $('#username').val();
+        var message = $('#message').val();
+
+        if (username == '' || message == '') {
+            alert('Please enter name and message!!');
+        } else {
+            //Gửi dữ liệu cho socket
+            socket.emit('send', { username: username, message: message });
+            $('#message').val('');
+        }
+    }
+})
